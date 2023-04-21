0294a9aaec6106a96ba481f178b91aee313c4f3c
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2023-04-21 12:26:42 +0300

    color palet added

---
+++ lib/utils/color_palet.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColorPalet {
+  static const MaterialColor font =
+      MaterialColor(0xfffcfacc, <int, Color>{100: Color(0xfffcfacc)});
+  static const MaterialColor primer =
+      MaterialColor(0xfff2b99e, <int, Color>{100: Color(0xfff2b99e)});
+  static const MaterialColor seconder =
+      MaterialColor(0xffbfe4fb, <int, Color>{100: Color(0xffbfe4fb)});
+  static const MaterialColor tersiyer =
+      MaterialColor(0xff01030a, <int, Color>{100: Color(0xff01030a)});
+}
